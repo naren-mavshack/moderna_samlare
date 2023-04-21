48394e249d3a21c7ebe2c8cadb793a8675f19203
Naren <narendra.vaishnav@mavshack.com>
2023-04-21 10:28:16 +0530

    home page art work api fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "edd1e54a67e13fe601da483c8d08e859",
-"index.html": "06801a8006f3ea9d893d3b02856ca4b7",
-"/": "06801a8006f3ea9d893d3b02856ca4b7",
-"main.dart.js": "ee86f6f4168e9f40619302ea481d2cc7",
+"index.html": "07e1fc684b8416e7c99616f843b905cf",
+"/": "07e1fc684b8416e7c99616f843b905cf",
+"main.dart.js": "4a1788b0cad5dac0077915acabeff112",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
