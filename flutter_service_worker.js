1843d15284ce2f0ed6435a26536c7079e2d97ff3
Naren <narendra.vaishnav@mavshack.com>
2023-04-26 17:15:50 +0530

    code merged for market page

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "edd1e54a67e13fe601da483c8d08e859",
-"index.html": "faa995eddf862e3549ad9fa0dc2c4fa9",
-"/": "faa995eddf862e3549ad9fa0dc2c4fa9",
-"main.dart.js": "81119f0a6e115a49d7d6786102beeda5",
+"index.html": "d94727e8eb8ce76eefc1f034a8986b04",
+"/": "d94727e8eb8ce76eefc1f034a8986b04",
+"main.dart.js": "37d10c5c0fda7f47c266968c16d7f04a",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
